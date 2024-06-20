2d11013e0af32b1887de985723b2c2a7d59fdcf1
Dana Gunawan <dewakadekdana196@gmail.com>
2024-06-20 23:05:04 +0800

    menambahkan tampilan admin dari admin lte

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Admin
+
+Route::get('admin',function(){return view('admin/auth/login');});
